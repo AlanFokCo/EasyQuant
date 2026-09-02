1922d7c044be07ac31c722ef4b123b69de3b9d6a
AlanFokCo <alanfok2868@gmail.com>
2026-09-02 22:41:02 +0800

    style: format workflow lock regression test

--- tests/test_workflow_hygiene.py
+++ tests/test_workflow_hygiene.py
@@ -24,5 +24,8 @@ def test_python310_workflows_install_the_hash_locked_dependency_closure():
     for workflow_name in ("test.yml", "eqlib-evaluator.yml"):
         workflow = (ROOT / ".github" / "workflows" / workflow_name).read_text()
 
-        assert "pip install --require-hashes -r requirements/constraints-py310.txt" in workflow
+        assert (
+            "pip install --require-hashes -r requirements/constraints-py310.txt"
+            in workflow
+        )
         assert 'pip install --no-deps -e ".[dev]"' in workflow
